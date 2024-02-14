ed182764216f3507210c146aaef9a62ddfd3496e
Evgeniy Anishchenko <eg.anishchenko@yandex.ru>
2024-02-14 15:12:57 +0300

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float _rotateSpeed = 15f;
+    [SerializeField] private float _moveSpeed = 5f;
+
+    private const string Horizontal = nameof(Horizontal);
+    private const string Vertical = nameof(Vertical);
+
+    private void Update()
+    {
+        Rotate();
+        Move();
+    }
+
+    private void Move()
+    {
+        float _movement = Input.GetAxis(Vertical);
+
+        transform.Translate(_movement * _moveSpeed * Time.deltaTime * Vector3.forward);
+    }
+
+    private void Rotate()
+    {
+        float rotation = Input.GetAxis(Horizontal);
+
+        transform.Rotate(rotation * _rotateSpeed * Time.deltaTime * Vector3.up);
+    }
+}
